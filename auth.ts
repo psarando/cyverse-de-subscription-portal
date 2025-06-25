ef946e90d3bbe024f80e49efe9836e735df9fbe7
Paul Sarando <psarando@cyverse.org>
2025-06-24 18:55:14 -0700

    CORE-2063 Initial `next-auth` integration for Keycloak support

---
+++ auth.ts
@@ -0,0 +1,30 @@
+import type {
+    GetServerSidePropsContext,
+    NextApiRequest,
+    NextApiResponse,
+} from "next";
+import type { NextAuthOptions } from "next-auth";
+import { getServerSession } from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
+
+// The `NextAuth` config, for use in `app/api/auth/[...nextauth]/route.ts`
+export const config = {
+    debug: process.env.NODE_ENV !== "production",
+    providers: [
+        KeycloakProvider({
+            clientId: process.env.SP_KEYCLOAK_CLIENT_ID || "",
+            clientSecret: process.env.SP_KEYCLOAK_CLIENT_SECRET || "",
+            issuer: process.env.SP_KEYCLOAK_ISSUER,
+        }),
+    ],
+} satisfies NextAuthOptions;
+
+// Use it in server contexts
+export function auth(
+    ...args:
+        | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+        | [NextApiRequest, NextApiResponse]
+        | []
+) {
+    return getServerSession(...args, config);
+}
