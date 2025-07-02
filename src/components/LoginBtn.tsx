10f8b89ec60a58bab78fca360c2470b50fb4bfd1
Paul Sarando <psarando@cyverse.org>
2025-07-01 20:15:48 -0700

    CORE-2065 Initial integration with MUI

--- src/components/LoginBtn.tsx
+++ src/components/LoginBtn.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSession, SessionProvider, signIn, signOut } from "next-auth/react";
+import Button from "@mui/material/Button";
 
 const LoginBtn = () => {
     const { data: session } = useSession();
@@ -8,22 +9,16 @@ const LoginBtn = () => {
     return session ? (
         <div>
             Signed in as {session.user?.email} <br />
-            <button
-                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                onClick={() => signOut()}
-            >
+            <Button variant="contained" onClick={() => signOut()}>
                 Sign out
-            </button>
+            </Button>
         </div>
     ) : (
         <div>
             Not signed in <br />
-            <button
-                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                onClick={() => signIn("keycloak")}
-            >
+            <Button variant="contained" onClick={() => signIn("keycloak")}>
                 Sign in
-            </button>
+            </Button>
         </div>
     );
 };
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    colorSchemes: {
+        dark: true,
+    },
+    typography: {
+        fontFamily: "var(--font-roboto)",
+    },
+});
+
+export default theme;
