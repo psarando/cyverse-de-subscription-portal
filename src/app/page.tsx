4a8acfcb256e0da2af3e6b934eb980620048958a
Paul Sarando <psarando@cyverse.org>
2025-07-02 19:46:56 -0700

    CORE-2065 Update CyVerse logo
    Replace the CyVerse logo with one that has a transparent background.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default async function Home() {
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
                 <Image
-                    src="/cyverse_logo_0.png"
+                    src="/cyverse_logo_2.png"
                     alt="CyVerse logo"
-                    width={500}
-                    height={100}
+                    width={836}
+                    height={170}
                     priority
                 />
                 <div className="flex gap-4 items-center flex-col">
