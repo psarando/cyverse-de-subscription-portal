6126796d9bfeffdad82eda99f0f30a79e47a0c0e
Paul Sarando <psarando@cyverse.org>
2025-07-08 16:15:26 -0700

    CORE-2065 Import primary theme options from DE

---
+++ src/components/theme/CyVersePalette.js
@@ -0,0 +1,21 @@
+const CyVersePalette = {
+    white: "#ffffff",
+    lightSilver: "#e2e2e2",
+    silver: "#a5a4a4",
+    blueGrey: "#48515f",
+    bgGray: "#f4f4f4",
+    black: "#000000",
+    darkNavy: "#142248",
+    navy: "#004471",
+    cobalt: "#0971AB", // primary
+    sky: "#99D9EA",
+    yellow: "#F7D21E",
+    gold: "#F8981D",
+    violet: "#AA2173",
+    indigo: "#4A2E8D",
+    leaf: "#378F43",
+    grass: "#7CB342",
+    alertRed: "#AF0404",
+};
+
+export default CyVersePalette;
