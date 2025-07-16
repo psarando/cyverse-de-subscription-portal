cf506ba86450786b3c7e772e935184b2485afc49
Paul Sarando <psarando@cyverse.org>
2025-07-15 19:18:07 -0700

    CORE-2063 CORE-2066 Disable refetchOnWindowFocus
    Disable `refetchOnWindowFocus` for sessions and queries,
    and disable query retries.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ children }: { children: React.ReactNode }) {
-    const queryClient = new QueryClient();
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: { refetchOnWindowFocus: false, retry: false },
+        },
+    });
 
     return (
-        <SessionProvider>
+        <SessionProvider
+            // Disable session re-fetches when window is focused
+            refetchOnWindowFocus={false}
+        >
             <QueryClientProvider client={queryClient}>
                 {children}
             </QueryClientProvider>
