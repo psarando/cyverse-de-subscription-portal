1a426303053ffb042c189e2f47cde1f53b00acb1
Paul Sarando <psarando@cyverse.org>
2025-08-15 19:43:33 -0700

    CORE-2073 Parse error messages from Authorize.net API 200 responses

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -38,28 +38,28 @@ export async function POST(request: NextRequest) {
         body: JSON.stringify(createTransactionRequest),
     });
 
-    if (!response.ok) {
-        const text = await response.text();
+    const status = response.status;
+    const text = await response.text();
 
-        let errorJson;
-        try {
-            errorJson = JSON.parse(text);
-        } catch {
-            console.error("non-JSON error response", {
-                status: response.status,
-                url: authorizeNetApiEndpoint,
-                text,
-            });
-        }
+    let responseJson;
+    try {
+        responseJson = JSON.parse(text);
+    } catch {
+        console.error("non-JSON response", {
+            status,
+            url: authorizeNetApiEndpoint,
+            text,
+        });
+    }
 
+    if (!response.ok || responseJson?.messages?.resultCode === "Error") {
         return NextResponse.json(
-            errorJson || { message: response.statusText },
+            responseJson?.messages || { message: response.statusText },
             {
-                status: response.status || 500,
+                status: !response.ok && status ? status : 500,
             },
         );
     }
 
-    const data = await response.json();
-    return NextResponse.json(data);
+    return NextResponse.json(responseJson);
 }
