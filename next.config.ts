af8040923857df481898bb9dd684abd47ff57fc6
Sarah Roberts <sonoran.sarah@gmail.com>
2025-09-10 17:12:02 -0700

    CORE-2085: added deployment and build scripts and updated the configuration code

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import { PHASE_PRODUCTION_BUILD } from "next/constants";
 
 // appConfiguration defines the supported configuration settings for the application.
 const appConfiguration = {
@@ -47,55 +47,56 @@ const appConfiguration = {
 };
 
 // loadConfig loads the runtime configuration from the environment based on the definitions in `appConfiguration`.
-function loadConfig() {
+function loadConfig(phase: string) {
     let configurationError = false;
     const publicRuntimeConfig: Record<string, string | undefined> = {};
     const serverRuntimeConfig: Record<string, string | undefined> = {};
 
-    for (const [
-        key,
-        { variable, required, isPublic, defaultValue },
-    ] of Object.entries(appConfiguration)) {
-        const defined =
-            variable in process.env ||
-            typeof process.env[variable] !== "undefined";
+    if (phase !== PHASE_PRODUCTION_BUILD) {
+        for (const [
+            key,
+            { variable, required, isPublic, defaultValue },
+        ] of Object.entries(appConfiguration)) {
+            const defined =
+                variable in process.env ||
+                typeof process.env[variable] !== "undefined";
 
-        // Log an error if a required configuration setting isn't defined.
-        if (required && !defined) {
-            console.log(
-                `configuration error [${key}]: ${variable} is not defined`,
-            );
-            configurationError = true;
-            continue;
-        }
+            // Log an error if a required configuration setting isn't defined.
+            if (required && !defined) {
+                console.log(
+                    `configuration error [${key}]: ${variable} is not defined`,
+                );
+                configurationError = true;
+                continue;
+            }
 
-        // Get the value of the configuration setting.
-        const value = defined ? process.env[variable] : defaultValue;
+            // Get the value of the configuration setting.
+            const value = defined ? process.env[variable] : defaultValue;
 
-        // Store the configuration setting.
-        if (isPublic) {
-            publicRuntimeConfig[key] = value;
-        } else {
-            serverRuntimeConfig[key] = value;
+            // Store the configuration setting.
+            if (isPublic) {
+                publicRuntimeConfig[key] = value;
+            } else {
+                serverRuntimeConfig[key] = value;
+            }
         }
     }
 
     return { configurationError, publicRuntimeConfig, serverRuntimeConfig };
 }
 
-// Load the configuration.
-const { configurationError, publicRuntimeConfig, serverRuntimeConfig } =
-    loadConfig();
-if (configurationError) {
-    console.log("a configuration error was detected: exiting");
-    process.exit(1);
+// init initializes the application and exits if an initialization error is encountered.
+function init(phase: string) {
+    const { configurationError, publicRuntimeConfig, serverRuntimeConfig } = loadConfig(phase)
+    if (configurationError) {
+        process.exit(1)
+    }
+
+    return {
+        publicRuntimeConfig,
+        serverRuntimeConfig,
+        logging: { fetches: { fullUrl: true } },
+    }
 }
 
-const nextConfig: NextConfig = {
-    publicRuntimeConfig,
-    serverRuntimeConfig,
-    // Full URL logging to the console when running in dev mode.
-    logging: { fetches: { fullUrl: true } },
-};
-
-export default nextConfig;
+export default init;
