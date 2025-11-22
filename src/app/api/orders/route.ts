612cbf4b1ac7af726f1c45d5ddb1ef7e9a0b5b89
Paul Sarando <psarando@cyverse.org>
2025-11-21 17:06:52 -0700

    CORE-2069 Validate sorting params in `/api/orders` route

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -16,8 +16,27 @@ export async function GET(request: NextRequest) {
     }
 
     const params = request.nextUrl.searchParams;
-    const orderBy = params.get("orderBy") as PurchaseSortField;
-    const orderDir = params.get("orderDir") as OrderDir;
+    const orderBy = params.get("orderBy")?.toLowerCase() as PurchaseSortField;
+    const orderDir = params.get("orderDir")?.toLowerCase() as OrderDir;
+
+    if (orderBy && !Object.values(PurchaseSortField).includes(orderBy)) {
+        return NextResponse.json(
+            {
+                error_code: "ERR_BAD_REQUEST",
+                message: "Invalid orderBy field",
+            },
+            { status: 400 },
+        );
+    }
+    if (orderDir && !Object.values(OrderDir).includes(orderDir)) {
+        return NextResponse.json(
+            {
+                error_code: "ERR_BAD_REQUEST",
+                message: "Invalid orderDir",
+            },
+            { status: 400 },
+        );
+    }
 
     const orders = await getPurchasesByUsername(username, orderBy, orderDir);
 
